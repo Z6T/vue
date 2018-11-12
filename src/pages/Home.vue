c88a4007d8c13fac1213497d9a8d86db56cc3fb9
Z6T <1037457278@qq.com>
2018-11-12 23:56:00 +0800

    change

---
+++ src/pages/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <home-header></home-header>
+</template>
+
+<script>
+import HomeHeader from '@/pages/header/Header'
+export default {
+  name: 'Home',
+  components: {
+    HomeHeader
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/header/Header.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="header">
+      <div class="header-left">
+        <div class="iconfont back-icon">&#xe624;</div>
+      </div>
+      <div class="header-input"></div>
+      <div class="header-right"></div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style scoped lang="stylus">
+  @import "~styles/vars.styl"
+  .header
+    display: flex
+    line-height torem(44)
+    height torem(44)
+    background-color $bgcolor
+    color #fff
+    .header-left
+      float left
+      width torem(32)
+      .back-icon
+        text-align center
+        font-size torem(18)
+
+</style>
