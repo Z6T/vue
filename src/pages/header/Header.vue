4a90bd224585b74d2553173b974389d0d534e6b3
Z6T <1037457278@qq.com>
2018-11-13 00:16:08 +0800

    change

--- src/pages/header/Header.vue
+++ src/pages/header/Header.vue
@@ -3,8 +3,13 @@
       <div class="header-left">
         <div class="iconfont back-icon">&#xe624;</div>
       </div>
-      <div class="header-input"></div>
-      <div class="header-right"></div>
+      <div class="header-input">
+        <span class="iconfont">&#xe632;</span>
+        输入城市/景点/游玩主题
+      </div>
+      <div class="header-right">
+        北京<span class="iconfont arrow-icon">&#xe64a;</span>
+      </div>
     </div>
 </template>
 
@@ -24,9 +29,26 @@ export default {
     color #fff
     .header-left
       float left
+      margin-right torem(8)
       width torem(32)
       .back-icon
         text-align center
         font-size torem(18)
-
+        font-weight bold
+    .header-input
+      flex 1
+      margin-top torem(7)
+      padding-left torem(10)
+      line-height torem(30)
+      height torem(30)
+      background #fff
+      border-radius: .1rem
+      color #e4e7ea
+    .header-right
+      float right
+      width torem(66)
+      text-align center
+      .arrow-icon
+        font-size .24rem
+        margin-left: 0.04rem;
 </style>
