589f76723aaf18ec8783fbba7a5e405c9ffa6a4d
Z6T <1037457278@qq.com>
2018-11-19 09:16:06 +0800

    citylist complete

--- src/components/city/CityAlphabet.vue
+++ src/components/city/CityAlphabet.vue
@@ -1,5 +1,8 @@
 <template>
   <ul class="list" >
+    <li  v-show="tipDivShow">
+      <div class="showletterdiv">{{ thisLetter}}</div>
+    </li>
     <li
       :key="item"
       v-for="item in letterarr"
@@ -18,7 +21,11 @@ export default {
   name: 'CityAlphabet',
   data () {
     return {
-      touchState: false
+      touchState: false,
+      tipDivShow: false,
+      thisLetter: 'A',
+      AstartY: '',
+      timer: null
     }
   },
   props: {
@@ -33,14 +40,26 @@ export default {
       this.touchState = true
     },
     handleTouchMove (e) {
-      debugger
-      var AstartY = this.$refs['A'][0].offsetTop
-      var thisletterY = e.touches[0].clientY
-      var index = Math.floor((thisletterY - AstartY - 79) / 15)
-      console.log(this.letterarr[index])
+      if (this.touchState) { // 节流操作
+        if (!this.timer) {
+          clearTimeout(this.timer)
+        }
+        setTimeout(() => {
+          this.tipDivShow = true // 滑动时候提示字母位置
+          var thisletterY = e.touches[0].clientY
+          var index = Math.floor((thisletterY - this.AstartY - 79) / 15)
+          if (index >= 0 && index < this.letterarr.length) {
+            this.thisLetter = this.letterarr[index]
+            Bus.$emit('scrollEle', this.thisLetter)
+          }
+        }, 16)
+      }
     },
     handleTouchEnd () {
       this.touchState = false
+      setTimeout(() => {
+        this.tipDivShow = false
+      }, 1000)
     }
   },
   computed: {
@@ -51,11 +70,27 @@ export default {
       }
       return letters
     }
+  },
+  updated () {
+    this.AstartY = this.$refs['A'][0].offsetTop
   }
 }
 </script>
 
 <style scoped lang="stylus">
+.showletterdiv
+  position: absolute;
+  top: 50%;
+  right: 20px;
+  margin-top: -15px;
+  width: 30px;
+  background: rgba(208, 192, 192, 0.6);
+  height: 30px;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
 .list
   display: flex
   flex-direction column
